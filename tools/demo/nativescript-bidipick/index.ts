f4563fc60077515ec54b48bf54191fb4eacb5587
guymatus <guymatus@gmail.com>
2022-08-13 16:41:56 +0300

    fix

--- tools/demo/nativescript-bidipick/index.ts
+++
@@ -1,8 +0,0 @@
-import { DemoSharedBase } from '../utils';
-import {} from '@nativescript/nativescript-bidipick';
-
-export class DemoSharedNativescriptBidipick extends DemoSharedBase {
-  testIt() {
-    console.log('test nativescript-bidipick!');
-  }
-}
