43b9a5f9b31c3bba831928c4d5d1e662aa3450c2
guymatus <guymatus@gmail.com>
2022-08-13 12:45:11 +0300

    bugfix

--- apps/demo/src/plugin-demos/nativescript-bidipick.ts
+++ apps/demo/src/plugin-demos/nativescript-bidipick.ts
@@ -1,6 +1,5 @@
 import { Observable, EventData, Page } from '@nativescript/core';
 import { DemoSharedNativescriptBidipick } from '@demo/shared';
-import {} from '@nativescript/nativescript-bidipick';
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object;
